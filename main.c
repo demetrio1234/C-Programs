f7fc827c8637f3cfb42b65954378844940907033
demetrio1234 <dem.iaria@gmail.com>
2025-04-12 09:14:45 +0200

    Create a function Add taking 2 parameters

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 
+// Function prototype for add
+int add(int x, int y);
+
 int main()
 {
     int a = 42;
-    printf("%d", a);
+    printf("Age: %d", a);
+
+    int res = add(10, 15);
+
+    printf("\n%d", res);
+
     return a - a;
+}
+
+int add(int x, int y)
+{
+    return x + y;
 }
