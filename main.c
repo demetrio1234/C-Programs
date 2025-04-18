d533c74c8d4a641b6817a3e4d9b165d20719a2cf
demetrio1234 <dem.iaria@gmail.com>
2025-04-18 14:24:44 +0200

    Add function prototypes and some examüle function

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
-// Online C compiler to run C program online
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
-// Function prototype for add
 int add(int x, int y);
+void printSum(int x, int y);
 
 int main()
 {
+    printSum(5, 6);
+
     int a = 42;
     printf("Age: %d", a);
 
@@ -13,10 +16,21 @@ int main()
 
     printf("\n%d", res);
 
+    char v[] = "12345";
+    char y[] = "12345";
+
+    int ret = strcmp(v, y);
+
     return a - a;
 }
 
 int add(int x, int y)
 {
     return x + y;
+}
+
+void printSum(int x, int y)
+{
+    int sum = x + y;
+    printf("%d", sum);
 }
